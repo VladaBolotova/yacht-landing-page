59dcb6cb60ab700568558950fb6a986d184ae64a
VladaBolotova <vladislavabolotova@gmail.com>
2023-05-12 17:57:29 -0400

    css

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => (
 
       <img
   src="/ph.jpg"
-  className="w-full h-full sm:h-[500px] h-[350px] object-cover rounded-[24px] sm:pr-[10px] "
+  className="w-full h-full sm:h-[500px] h-[350px] object-cover rounded-[24px] sm:pr-[10px] sm:px-[10px] "
   />
 
  
